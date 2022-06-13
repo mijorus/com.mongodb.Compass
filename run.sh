059ad8184b9c9749ece2a8a6eb8a99fb2d31deae
Lorenzo Paderi <lorenzo.mijorus@gmail.com>
2022-06-13 18:31:08 +0200

    fix zypak errors

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-flatpak-builder build/ com.mongodb.Compass.json --force-clean --user
-flatpak-builder --run build/ com.mongodb.Compass.json run.sh
+flatpak-builder build/ com.mongodb.Compass.json --force-clean --user --install
+flatpak run com.mongodb.Compass
